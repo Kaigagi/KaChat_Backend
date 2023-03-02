ccc14a95081dd77ccf4bde0447556e51945a0c02
kaigagi <kaigagipro@gmail.com>
2023-03-03 10:34:31 +1100

    init app

---
+++ src/main/java/kaigagi/KaChat/KaChatApplication.java
@@ -0,0 +1,13 @@
+package kaigagi.KaChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kaigagi/KaChat/KaChatApplicationTests.java
@@ -0,0 +1,13 @@
+package kaigagi.KaChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
